76629ddc149ffc4a4253a9f9fb858e7a1ac9a384
Lalith Kota <kotalalith@gmail.com>
2022-05-11 10:24:13 +0530

    [MOSIP-21619] Removed beneficiary.id_number, base_id=firstname

--- openg2p_program/models/openg2p_program_enrollment_import.py
+++ openg2p_program/models/openg2p_program_enrollment_import.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import itertools
+import time
 
 from odoo import api, models, fields
 from odoo.exceptions import ValidationError
@@ -8,12 +9,11 @@ from odoo.exceptions import ValidationError
 _logger = logging.getLogger(__name__)
 
 should_create_beneficiary = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_SHOULD_CREATE_BENEFICIARY","false")
-beneficiary_base_id_type = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_BENEFICIARY_BASE_ID", None)
 beneficiary_base_id_label = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_BENEFICIARY_BASE_ID_LABEL", "Related Base ID")
-create_beneficiary_default_street = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_CREATE_BENEFICIARY_DEFAULT_STREET", "Electronic City")
-create_beneficiary_default_city = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_CREATE_BENEFICIARY_DEFAULT_CITY", "Bengaluru")
-create_beneficiary_default_state = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_CREATE_BENEFICIARY_DEFAULT_STATE", "Karnataka")
-create_beneficiary_default_country = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_CREATE_BENEFICIARY_DEFAULT_COUNTRY", "India")
+create_beneficiary_default_street = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_CREATE_BENEFICIARY_DEFAULT_STREET", "-")
+create_beneficiary_default_city = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_CREATE_BENEFICIARY_DEFAULT_CITY", "-")
+create_beneficiary_default_state = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_CREATE_BENEFICIARY_DEFAULT_STATE", 593)
+create_beneficiary_default_country = os.getenv("PROGRAM_ENROLLMENT_ON_IMPORT_CREATE_BENEFICIARY_DEFAULT_COUNTRY", 104)
 
 
 class ProgramEnrollmentImport(models.Model):
@@ -25,36 +25,19 @@ class ProgramEnrollmentImport(models.Model):
     total_ammount = fields.Float(
         string="Total Remuneration", required=False, default=0.0
     )
-    related_ben_base_id = fields.Char(compute="_compute_related_base_id", store=False, string=beneficiary_base_id_label)
-    related_ben_street = fields.Char(related="beneficiary_id.street")
-    related_ben_city = fields.Char(related="beneficiary_id.city")
-    related_ben_state = fields.Many2one(related="beneficiary_id.state_id")
-    related_ben_country = fields.Many2one(related="beneficiary_id.country_id")
     related_ben_programs = fields.Many2many(string="Beneficiary Listed Programs", related="beneficiary_id.program_ids", store=False)
-
-    @api.depends("beneficiary_id")
-    def _compute_related_base_id(self):
-        for record in self:
-            for iden in record.beneficiary_id.identities:
-                if iden.category_id.code == beneficiary_base_id_type:
-                    _logger.info("Identity FOUND!! "+str(iden.name))
-                    record.related_ben_base_id = iden.name
+    related_ben_base_id = fields.Char(string=beneficiary_base_id_label, related="beneficiary_id.firstname", store=False)
 
     @api.multi
     def import_models(self, fields, columns, options, data_generator, dryrun=False):
-        all_data = list(data_generator)
-        columns = all_data[0]
-        ben_base_id_cat = None
-        if beneficiary_base_id_type:
-            ben_base_id_cat = self.env["openg2p.beneficiary.id_category"].search([("code", "=", beneficiary_base_id_type)], limit=1)
-            if not ben_base_id_cat or ben_base_id_cat.name not in columns:
-                return {
-                    'messages': [{
-                        'type': 'error',
-                        'message': 'Improperly Configured on import base Id. Or Base Id not found in Data',
-                        'record': False,
-                    }]
-                }
+        if beneficiary_base_id_label.lower() not in columns:
+            return {
+                'messages': [{
+                    'type': 'error',
+                    'message': beneficiary_base_id_label + ' not found in Data',
+                    'record': False,
+                }]
+            }
         
         ben_error_count = 0
         ben_merge_count = 0
@@ -65,73 +48,96 @@ class ProgramEnrollmentImport(models.Model):
         total_count = 0
         error_messages = []
         success_ids = []
-        for row in all_data[1:]:
+        time_initial = time.time()
+        for row in itertools.islice(data_generator,1,None):
             total_count += 1
+            if total_count == 1:
+                _logger.info("PROGRAM_ENROL_IMPORT. No of Rows updated: %d .timestamp: %s" % (total_count, str(time.time()-time_initial)))
+            elif total_count == 10:
+                _logger.info("PROGRAM_ENROL_IMPORT. No of Rows updated: %d .timestamp: %s" % (total_count, str(time.time()-time_initial)))
+            elif total_count == 100:
+                _logger.info("PROGRAM_ENROL_IMPORT. No of Rows updated: %d .timestamp: %s" % (total_count, str(time.time()-time_initial)))
+            elif total_count == 1000:
+                _logger.info("PROGRAM_ENROL_IMPORT. No of Rows updated: %d .timestamp: %s" % (total_count, str(time.time()-time_initial)))
+            elif total_count == 10000:
+                _logger.info("PROGRAM_ENROL_IMPORT. No of Rows updated: %d .timestamp: %s" % (total_count, str(time.time()-time_initial)))
+            elif total_count == 50000:
+                _logger.info("PROGRAM_ENROL_IMPORT. No of Rows updated: %d .timestamp: %s" % (total_count, str(time.time()-time_initial)))
+                break
             row_data = dict(zip(columns, row))
 
-            program_name = row_data[self._fields["program_id"].string]
-            enrol_date_start = row_data[self._fields["date_start"].string]
-            enrol_date_end = row_data[self._fields["date_end"].string]
-            enrol_program_ammount = row_data[self._fields["program_ammount"].string]
-            enrol_total_ammount = row_data[self._fields["total_ammount"].string]
-            enrol_state = row_data[self._fields["state"].string]
+            enrol_program_name = row_data[self._fields["program_id"].string.lower()]
+            enrol_date_start = row_data[self._fields["date_start"].string.lower()]
+            enrol_date_end = row_data[self._fields["date_end"].string.lower()]
+            enrol_program_ammount = row_data[self._fields["program_ammount"].string.lower()]
+            enrol_total_ammount = row_data[self._fields["total_ammount"].string.lower()]
+            enrol_state = row_data[self._fields["state"].string.lower()]
 
             existing_bens = None
 
             # checking if beneficiary exists and creating if required
-            if beneficiary_base_id_type:
-                ben_id_from_data = row_data[ben_base_id_cat.name]
-                existing_bens = self.env["openg2p.beneficiary.id_number"].search([("name", "=", ben_id_from_data),("category_id","=", ben_base_id_cat.id)])
-                if len(existing_bens) == 0 and should_create_beneficiary != "true":
+            ben_id_from_data = row_data[beneficiary_base_id_label.lower()]
+            existing_bens = self.env["openg2p.beneficiary"].search([("firstname", "=", ben_id_from_data)])
+            if len(existing_bens) == 0 and should_create_beneficiary != "true":
+                error_messages.append({
+                    'type': 'error',
+                    'message': 'Beneficiary not found',
+                    'record': str(row_data)
+                })
+                ben_error_count += 1
+                continue
+            elif len(existing_bens) == 0 and should_create_beneficiary == "true":
+                try:
+                    existing_bens = self.create_ben_with_data(ben_id_from_data, row_data)
+                    ben_create_count += 1
+                except Exception as e:
                     error_messages.append({
                         'type': 'error',
-                        'message': 'Beneficiary not found',
+                        'message': 'Error Creating Beneficiary: ' + str(e),
                         'record': str(row_data)
                     })
                     ben_error_count += 1
-                    continue
-                elif len(existing_bens) == 0 and should_create_beneficiary == "true":
-                    try:
-                        existing_bens = self.create_ben_with_data(ben_id_from_data, ben_base_id_cat, row_data)
-                        ben_create_count += 1
-                    except Exception as e:
-                        error_messages.append({
-                            'type': 'error',
-                            'message': 'Error Creating Beneficiary: ' + str(e),
-                            'record': str(row_data)
-                        })
-                        ben_error_count += 1
-                elif len(existing_bens) == 1 and should_create_beneficiary == "true":
-                    try:
-                        self.merge_ben_with_data(existing_bens, row_data)
-                        ben_merge_count += 1
-                    except Exception as e:
-                        error_messages.append({
-                            'type': 'error',
-                            'message': 'Error merging Beneficiary: ' + str(e),
-                            'record': str(row_data)
-                        })
-                        ben_error_count += 1
-                elif len(existing_bens) == 1:
-                    pass
-                # else len(existing_bens) is >1 or <0
-                else:
-                    raise ValidationError("Improper Beneficiary Data found in db")
+            elif len(existing_bens) == 1 and should_create_beneficiary == "true":
+                # try:
+                #     self.merge_ben_with_data(existing_bens, row_data)
+                #     ben_merge_count += 1
+                # except Exception as e:
+                #     error_messages.append({
+                #         'type': 'error',
+                #         'message': 'Error merging Beneficiary: ' + str(e),
+                #         'record': str(row_data)
+                #     })
+                #     ben_error_count += 1
+                pass
+            elif len(existing_bens) == 1:
+                pass
+            # else len(existing_bens) is >1 or <0
+            else:
+                raise ValidationError("Improper Beneficiary Data found in db")
             
             # checking if the current program enrollment exists and creating/merging accordingly
-            program_id = self.env["openg2p.program"].search([("name", "=", program_name)], limit=1)
-            existing_enrols = self.search([("program_id", "=", program_id.id),("beneficiary_id", "=", existing_bens.beneficiary_id.id)])
+            if not enrol_program_name or not enrol_date_start:
+                continue
+            program_id = self.env["openg2p.program"].search([("name", "=", enrol_program_name)], limit=1)
+            existing_enrols = self.search([("program_id", "=", program_id.id),("beneficiary_id", "=", existing_bens.id)])
             if len(existing_enrols) == 0:
                 try:
-                    enrol = self.create({
+                    enrol_dict = {
                         "program_id": program_id.id,
-                        "beneficiary_id": existing_bens.beneficiary_id.id,
+                        "beneficiary_id": existing_bens.id,
                         "date_start": enrol_date_start,
-                        "date_end": enrol_date_end if enrol_date_end else program_id.date_end,
-                        "program_ammount": enrol_program_ammount,
-                        "total_ammount": enrol_total_ammount,
-                        "state": self.get_state_key_from_value(enrol_state)
-                    })
+                    }
+                    if enrol_date_end:
+                        enrol_dict["date_end"] = enrol_date_end
+                    elif program_id.date_end:
+                        enrol_dict["date_end"] = program_id.date_end
+                    if enrol_program_ammount:
+                        enrol_dict["program_ammount"] = enrol_program_ammount
+                    if enrol_total_ammount:
+                        enrol_dict["total_ammount"] = enrol_total_ammount
+                    enrol_dict["state"] = self.get_state_key_from_value(enrol_state) if enrol_state else "open"
+                    
+                    enrol = self.create(enrol_dict)
                     success_ids.append(enrol.id)
                     enrol_create_count += 1
                 except Exception as e:
@@ -143,15 +149,22 @@ class ProgramEnrollmentImport(models.Model):
                     enrol_error_count += 1
             elif len(existing_enrols) == 1:
                 try:
-                    existing_enrols.write({
+                    enrol_dict = {
                         "program_id": program_id.id,
-                        "beneficiary_id": existing_bens.beneficiary_id.id,
+                        "beneficiary_id": existing_bens.id,
                         "date_start": enrol_date_start,
-                        "date_end": enrol_date_end if enrol_date_end else program_id.date_end,
-                        "program_ammount": enrol_program_ammount,
-                        "total_ammount": enrol_total_ammount,
-                        "state": self.get_state_key_from_value(enrol_state)
-                    })
+                    }
+                    if enrol_date_end:
+                        enrol_dict["date_end"] = enrol_date_end
+                    elif program_id.date_end:
+                        enrol_dict["date_end"] = program_id.date_end
+                    if enrol_program_ammount:
+                        enrol_dict["program_ammount"] = enrol_program_ammount
+                    if enrol_total_ammount:
+                        enrol_dict["total_ammount"] = enrol_total_ammount
+                    enrol_dict["state"] = self.get_state_key_from_value(enrol_state) if enrol_state else "open"
+
+                    existing_enrols.write(enrol_dict)
                     success_ids.append(existing_enrols.id)
                     enrol_merge_count += 1
                 except Exception as e:
@@ -174,42 +187,23 @@ class ProgramEnrollmentImport(models.Model):
             response['ids'] = success_ids
         return response
 
-    def create_ben_with_data(self, ben_base_id, ben_base_id_cat, row_data):
+    def create_ben_with_data(self, ben_base_id, row_data):
         data = self.prepare_data_ben(ben_base_id, row_data)
-        ben = self.env["openg2p.beneficiary"].create(data)
-        if beneficiary_base_id_type:
-            return self.env["openg2p.beneficiary.id_number"].create(
-                {
-                    "name": ben_base_id,
-                    "category_id": ben_base_id_cat.id,
-                    "beneficiary_id": ben.id
-                }
-            )
-        return None
+        return self.env["openg2p.beneficiary"].create(data)
     
-    def merge_ben_with_data(self, existing_ben_base_id, row_data):
-        data = self.prepare_data_ben(existing_ben_base_id.name, row_data)
-        existing_ben_base_id.beneficiary_id.write(data)
-        return existing_ben_base_id
+    def merge_ben_with_data(self, existing_ben, row_data):
+        data = self.prepare_data_ben(existing_ben.firstname, row_data)
+        existing_ben.write(data)
+        return existing_ben
     
     def prepare_data_ben(self, ben_base_id, row_data):
-        street_label = self.env["openg2p.beneficiary"]._fields["street"].string
-        state_label = self.env["openg2p.beneficiary"]._fields["state_id"].string
-        country_label = self.env["openg2p.beneficiary"]._fields["country_id"].string
-        city_label = self.env["openg2p.beneficiary"]._fields["city"].string
-        display_name_label = self._fields["beneficiary_id"].string
+        street = create_beneficiary_default_street
+        city = create_beneficiary_default_city
+        state_id = create_beneficiary_default_state
+        country_id = create_beneficiary_default_country
 
-        street = row_data[street_label] if street_label in row_data.keys() else create_beneficiary_default_street
-        city = row_data[city_label] if city_label in row_data.keys() else create_beneficiary_default_city
-        state = row_data[state_label].split(" (", 1)[0] if state_label in row_data.keys() else create_beneficiary_default_state
-        country = row_data[country_label] if country_label in row_data.keys() else create_beneficiary_default_country
-
-        country_id = self.env["res.country"].search([("name", "=", country)], limit=1).id
-        state_id = self.env["res.country.state"].search([("name", "=", state), ("country_id", "=", country_id)], limit=1).id
-
-        display_name = row_data[display_name_label] if display_name_label in row_data.keys() else (str(ben_base_id) + " _")
-        first_name = display_name.split(" ", 1)[0]
-        last_name = display_name.split(" ", 1)[1].split(" (", 1)[0]
+        first_name = str(ben_base_id)
+        last_name = "_"
         
         return {
             "firstname": first_name,
